979f268bc05077b1066ed23f12e26d6d70b5e8a8
brian <brian@morty.student.rit.edu>
2018-04-22 15:48:43 -0400

    Added IP testcase

--- server/server.py
+++ server/server.py
@@ -53,6 +53,8 @@ def clientthread(conn):
             print("[D] MAC Address: " + str(client_data[:17]) + '\n')
         elif conn_type == b'IPIPADDR':
             print("[D] IP Address: " + str(client_data[:15]) + '\n')
+        elif conn_type == b'USERNAME':
+            print("[D] Username: " + str(client_data[:32]) + '\n')
         elif conn_type == b'STORETXT':
             print("[D] Connection Type: STORE TEXT (STORETXT)")
             #TODO actually store the data
